4f070d6e0a22123693437712c8e037a6215530ea
alexpurhalo <alexpurhalo@gmail.com>
2016-07-18 15:14:42 +0300

    writing-browser-based-specs-with-capybara

---
+++ spec/features/restaurants_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'restaurants', js: true do
+  let(:restaurant_name) { 'Hamburger Joint' }
+  let!(:restaurant) { Restaurant.create(name: restaurant_name) }
+
+  it 'list restaurants' do
+    visit('/')
+    expect(page).to have_content(restaurant_name)
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,19 +2,38 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+# Requirement Files
 require 'spec_helper'
 require 'rspec/rails'
 require 'simplecov'
+require 'database_cleaner'
+require 'capybara/poltergeist'
 
+# Simple Cov Configurations
 SimpleCov.start
+
 
 ActiveRecord::Migration.maintain_test_schema!
 
+
+# Capybara Configurations
+Capybara.asset_host = 'http://localhost:4000'
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(
+      app,
+      debug: false,
+      inspector: true,
+      js_errors: true,
+      window_size: [1024, 768]
+  )
+end
+Capybara.javascript_driver = :poltergeist
+
+
+# Rspec Configurations
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  config.use_transactional_fixtures = true
-
+  # Database Cleaner
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.clean_with :truncation
@@ -26,9 +45,18 @@ RSpec.configure do |config|
     end
   end
 
+  # Capybara
+  config.include Capybara::DSL
+
+  # Default Stuff
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.use_transactional_fixtures = false
+
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
 end
 
+# Coverage Additional Abilities
 Rails.application.eager_load! if ENV['COVERAGE']
